29b0074bc885e021b11ec0c5dfc7be72306da8af
Rory <ranesbitt@live.co.uk>
2023-09-23 19:59:24 +0100

    chore(tempdir): update substitution to match #25233

--- src/nvim/fileio.c
+++ src/nvim/fileio.c
@@ -3293,8 +3293,8 @@ static void vim_mktempdir(void)
 
   (void)os_get_username(user, sizeof(user));
   // Usernames may contain slashes! #19240
-  memchrsub(user, '/', '_', sizeof(user));
-  memchrsub(user, '\\', '_', sizeof(user));
+  memchrsub(user, '/', '%', sizeof(user));
+  memchrsub(user, '\\', '%', sizeof(user));
 
   // Make sure the umask doesn't remove the executable bit.
   // "repl" has been reported to use "0177".
